77b9c4680b51a83a986f66f71da32c2fbac068c1
salolodin-arch <salolodin@gmail.com>
2026-07-23 13:49:28 -0700

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 ADMIN_BOT_TOKEN = os.getenv("ADMIN_BOT_TOKEN", "")
+PUBLIC_BOT_TOKEN = os.getenv("PUBLIC_BOT_TOKEN", "")
 ADMIN_CHAT_ID = int(os.getenv("ADMIN_CHAT_ID", "0"))
+PORT = int(os.getenv("PORT", "8000"))
 
-if not ADMIN_BOT_TOKEN or not ADMIN_CHAT_ID:
-    print("OGOHLANTIRISH: .env (yoki Railway Variables) da ADMIN_BOT_TOKEN / ADMIN_CHAT_ID to'liq emas!")
+if not ADMIN_BOT_TOKEN or not PUBLIC_BOT_TOKEN or not ADMIN_CHAT_ID:
+    print("OGOHLANTIRISH: .env faylida ADMIN_BOT_TOKEN / PUBLIC_BOT_TOKEN / ADMIN_CHAT_ID to'liq emas!")
