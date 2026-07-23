361257e9c3abe07b0be3f39669c4d59ca94366b5
salolodin-arch <salolodin@gmail.com>
2026-07-23 14:19:10 -0700

    Update config.py

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ ADMIN_BOT_TOKEN = os.getenv("ADMIN_BOT_TOKEN", "")
 PUBLIC_BOT_TOKEN = os.getenv("PUBLIC_BOT_TOKEN", "")
 ADMIN_CHAT_ID = int(os.getenv("ADMIN_CHAT_ID", "0"))
 PORT = int(os.getenv("PORT", "8000"))
+IMGBB_API_KEY = os.getenv("IMGBB_API_KEY", "")
 
 if not ADMIN_BOT_TOKEN or not PUBLIC_BOT_TOKEN or not ADMIN_CHAT_ID:
     print("OGOHLANTIRISH: .env faylida ADMIN_BOT_TOKEN / PUBLIC_BOT_TOKEN / ADMIN_CHAT_ID to'liq emas!")
